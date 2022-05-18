841548b842cc33b42cd898236ccbdf42641346f3
Iliyan Germanov <iliyan.germanov971@gmail.com>
2022-05-18 13:35:18 +0300

    Bump library version

--- frp/build.gradle.kts
+++ frp/build.gradle.kts
@@ -65,7 +65,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "com.ivy"
             artifactId = "frp"
-            version = "0.0.4"
+            version = "0.0.5"
 
             afterEvaluate {
                 from(components["release"])
