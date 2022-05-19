92c756934d822551d7ec965ee2fbd9f78843af1b
Iliyan Germanov <iliyan.germanov971@gmail.com>
2022-05-19 14:29:04 +0300

    Bump version

--- buildSrc/src/main/java/com/ivy/frp/buildsrc/dependencies.kt
+++ buildSrc/src/main/java/com/ivy/frp/buildsrc/dependencies.kt
@@ -24,7 +24,7 @@ object Project {
     object Lib {
         const val groupId = "com.ivy"
         const val artifactId = "frp"
-        const val version = "0.2.0"
+        const val version = "0.2.1"
     }
 
     //Compile SDK & Build Tools
